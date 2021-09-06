fefcfe283f44d9596a666521c45f2bdb8aa60cfa
Diego Virguez <davirguezc18@gmail.com>
2021-09-06 01:48:04 -0500

    first commit

---
+++ src/test/java/com/diego/asyncmethod/AsynchronousMethodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.asyncmethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchronousMethodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
